3cbee9b772c63fc2f6046f3a9bb63360e2fdf574
EzequielTartaglia <96756615+EzequielTartaglia@users.noreply.github.com>
2022-06-18 01:53:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ particlesJS(
     {
         "particles": {
           "number": {
-            "value": 1000,
+            "value": 600,
             "density": {
               "enable": false,
               "value_area": 10000
